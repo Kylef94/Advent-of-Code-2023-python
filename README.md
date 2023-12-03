a0d413ccf841f611f84589718e2e64b0120cf2f7
kylef94 <k.farrugia94@gmail.com>
2023-12-03 12:50:58 +0000

    added makeday.sh, solved day 3 and updated readme.md

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 This is my repository for my advent of code 2023 solutions, completed in Python. I am also learning Rust and may set up a seperate repo for my rust attempts if I get anywhere :)
 
+I also have a makeday.sh bash script included which generates a new folder and templates for each day.
+
 ### Summary
 
 Day 1 - Solved  
-Day 2 - Solved
+Day 2 - Solved  
+Day 3 - Solved
 
 Good luck to anyone else participating!!
---
+++ day3/part1.py
@@ -0,0 +1,89 @@
+from collections import deque
+
+def get_input():
+    data = []
+    with open("input.txt", "r") as f:
+        for l in f.readlines():
+            data.append(l.strip())
+    return data
+
+def get_num(row, c):
+    if row[c].isdigit():
+        tmp = deque()
+        tmp.append(row[c])
+        l = c - 1
+        r = c + 1
+        
+        while l >= 0 and row[l].isdigit():
+            tmp.appendleft(row[l])
+            l -= 1
+        
+        while r <= len(row) - 1 and row[r].isdigit():
+            tmp.append(row[r])
+            r += 1
+        res = "".join(tmp)
+        return int(res)
+    return None
+
+def search(data, r, c):
+    res = []
+    
+    #search left
+    if c > 0:
+        num = get_num(data[r], c-1)
+        if num: res.append(num)
+    
+    #search right
+    if c < len(data[0]):
+        num = get_num(data[r], c+1)
+        if num: res.append(num)
+        
+    #search above
+    if r > 0:
+        num = get_num(data[r-1], c)
+        if num: 
+            res.append(num)
+        else:
+            if c > 0:
+                num = get_num(data[r-1], c-1)
+                if num: res.append(num)
+            
+            if c < len(data[0]):
+                num = get_num(data[r-1], c+1)
+                if num: res.append(num)
+    
+    #search below
+    if r < len(data):
+        num = get_num(data[r+1], c)
+        if num: 
+            res.append(num)
+        else:
+            if c > 0:
+                num = get_num(data[r+1], c-1)
+                if num: res.append(num)
+            
+            if c < len(data[0]):
+                num = get_num(data[r+1], c+1)
+                if num: res.append(num)
+    return res
+    
+    
+def get_ans(data):
+    res = []
+    
+    for r in range(len(data)):
+        for c in range(len(data[0])):
+            cur = data[r][c]
+            
+            if cur == '.' or cur.isdigit():
+                continue
+            else:
+                res.extend(search(data, r, c))
+    return sum(res)
+    
+if __name__ == "__main__":
+    data = get_input()
+    print(get_ans(data))
+
+
+    
